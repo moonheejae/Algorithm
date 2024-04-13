070e48c280c8c4f99c204463ec5dd0a14a58ecb5
heejae <87752244+moonheejae@users.noreply.github.com>
2024-04-14 01:06:35 +0900

    [Bronze III] Title: 팩토리얼, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+ import java.util.*;
+
+public class Main {
+   public static void main(String[] args){
+
+        Scanner scanner = new Scanner( System.in );
+
+        int in = scanner.nextInt();
+
+        int result = 1;
+        for ( int i = 1; i <= in; i++ ){
+            result *= i;
+        }
+        System.out.println(result);
+   }
+}
