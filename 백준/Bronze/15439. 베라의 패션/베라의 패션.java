fb53006627e91163153f6662874c05f8f517acac
heejae <87752244+moonheejae@users.noreply.github.com>
2024-07-06 01:42:15 +0900

    [Bronze IV] Title: 베라의 패션, Time: 212 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        
+        System.out.print(num * num - num);
+    }
+}
